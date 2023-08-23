a2eca03a6f4196946b881ce5c5b6c9ed5d03e57e
Bailey Long <bailey-long@live.nmit.ac.nz>
2023-08-23 12:48:39 +1200

    fixed mime error when running from server

--- express-testing/server.js
+++ express-testing/server.js
@@ -1,25 +1,31 @@
-import express from 'express';
-import path from 'path';
-import fs from 'fs';
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
-    app.use((req, res, next) => {
-        console.log(`Received request from ${req.body}`)
-    });
+// Middleware to log request details
+app.use((req, res, next) => {
+    console.log(`Received request from ${req.url}`);
+    next(); // Don't forget to call next() to continue to the next middleware/route
+});
 
-    app.get('/', (req, res) => {
-        const htmlPage = path.join(__dirname, 'index.html')
-    });
+// 👇️ you need this line to serve static files
+app.use(express.static(__dirname + '/'));
 
-    fs.readFileSync(htmlPage, (error, htmlPage) => {
-        if (error){
+// Route handling for the root URL
+app.get('/', (req, res) => {
+    const htmlPage = path.join(__dirname, 'index.html');
+
+    fs.readFile(htmlPage, 'utf8', (error, htmlContent) => {
+        if (error) {
             console.log(error);
-            res.status(500).send("Internal Server Error")
-            return
+            res.status(500).send("Internal Server Error");
+            return;
         }
-        res.send(htmlPage)
+        res.send(htmlContent);
     });
+});
 
-//Listen for the middleware
+// Listen for incoming requests
 app.listen(3000, () => console.log('Server running on port 3000'));
