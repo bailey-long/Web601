3bed853fe3c6764763f120735b0203705120de83
Bailey Long <bailey-long@live.nmit.ac.nz>
2023-10-20 13:17:30 +1300

    more jwt stuff

--- jwt-testing/src/handlers.js
+++ jwt-testing/src/handlers.js
@@ -5,12 +5,35 @@ const jwtKey = "my_secret_key";
 const jwtExpirySeconds = 300;
 
 //NOTE - define the users database, which will be used to validate the user credentials.
+const users = {
+    user1: 'password1',
+    user2: 'password2'
+}
 
 const signIn = (req, res) => {
     //NOTE - Get credentials from JSON body
     const { username, password } = req.body;
-    
+    //REVIEW - return 400 status if username/password does not exist or if username/password is incorrect.
     if(!username || !password || users[username] !== password) {
+        return res.status(401).end();
+    }
+}
+
+//TODO - create a JWT token using the username in the payload
+const token = jwt.sign({ username }, jwtKey, {
+    algorithm: 'HS256',
+    expiresIn: jwtExpirySeconds
+});
+
+//NOTE - set the JWT as a cookie on the response
+res.cookie('token', token, { maxAge: jwtExpirySeconds * 1000 });
+res.end();
+
+const access = (req, res) => {
+    //NOTE - obtain the session token from the requests cookies, which come with every request
+    const token = req.cookies.token;
+    //TODO - if the cookie is not set, return an unauthorized error
+    if(!token) {
         return res.status(401).end();
     }
 }
